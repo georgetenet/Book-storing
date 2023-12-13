2027945e203aab10b809e2ca85d1bcf16a0e4078
mohammed <mohammedaliyu33@gmail.com>
2023-12-13 09:22:32 +0000

    controller class with html endpoints

--- src/main/java/com/example/book/controller/BookController.java
+++ src/main/java/com/example/book/controller/BookController.java
@@ -1,4 +1,42 @@
 package com.example.book.controller;
 
+import com.example.book.model.Book;
+import com.example.book.service.BookService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
+
+    private final BookService bookService = new BookService();
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.findAll();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable long id) {
+        return bookService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public void addBook(@RequestBody Book book) {
+        bookService.addBook(book);
+    }
+
+    @PutMapping("/{id}")
+    public void updateBook(@PathVariable long id, @RequestBody Book book) {
+        bookService.updateBook(id, book);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable long id) {
+        bookService.deleteBook(id);
+    }
 }
+
