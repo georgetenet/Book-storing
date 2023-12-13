7cf5300feeff81275569d67d7683871025837bc7
mohammed <mohammedaliyu33@gmail.com>
2023-12-13 09:22:08 +0000

    service logic for book class

--- src/main/java/com/example/book/service/BookService.java
+++ src/main/java/com/example/book/service/BookService.java
@@ -1,4 +1,34 @@
 package com.example.book.service;
 
+import com.example.book.model.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class BookService {
+    private final List<Book> books = new ArrayList<>();
+
+    public List<Book> findAll() {
+        return books;
+    }
+
+    public Optional<Book> findById(long id) {
+        return books.stream().filter(b -> b.getId() == id).findFirst();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void updateBook(long id, Book book) {
+        findById(id).ifPresent(b -> {
+            b.setTitle(book.getTitle());
+            b.setAuthor(book.getAuthor());
+        });
+    }
+
+    public void deleteBook(long id) {
+        books.removeIf(b -> b.getId() == id);
+    }
 }
